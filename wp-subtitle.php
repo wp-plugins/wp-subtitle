8c51b20b1031cd8b3ae1df5495a93629e898bcf7
husobj <husobj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 08:55:22 +0000

    Version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-subtitle/trunk@873897 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-subtitle.php
+++ wp-subtitle.php
@@ -6,7 +6,7 @@ Plugin URI: http://wordpress.org/plugins/wp-subtitle/
 Description: Adds a subtitle field to pages and posts. Possible to add support for custom post types.
 Author: Husani Oakley, Ben Huson
 Author URI: https://github.com/benhuson/wp-subtitle
-Version: 2.0.1
+Version: 2.1
 License: GPLv2
 */
 
@@ -28,25 +28,27 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-add_action( 'plugins_loaded', array( 'WPSubtitle', '_init' ) );
+// Plugin directory and url paths.
+define( 'WPSUBTITLE_BASENAME', plugin_basename( __FILE__ ) );
+define( 'WPSUBTITLE_SUBDIR', '/' . str_replace( basename( __FILE__ ), '', WPSUBTITLE_BASENAME ) );
+define( 'WPSUBTITLE_URL', plugins_url( WPSUBTITLE_SUBDIR ) );
+define( 'WPSUBTITLE_DIR', plugin_dir_path( __FILE__ ) );
+
+// Includes
+include_once( WPSUBTITLE_DIR . 'includes/deprecated.php' );
+
+// Include admin-only functionality
+if ( is_admin() ) {
+	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
+		// Load AJAX functions here if required...
+	} else {
+		require_once( WPSUBTITLE_DIR . 'admin/admin.php' );
+	}
+}
+
+add_action( 'init', array( 'WPSubtitle', '_add_default_post_type_support' ), 5 );
 
 class WPSubtitle {
-
-	/**
-	 * Init
-	 *
-	 * @since  2.0
-	 * @internal
-	 *
-	 * @uses  WPSubtitle::_add_default_post_type_support()
-	 * @uses  WPSubtitle::_add_meta_boxes()
-	 * @uses  WPSubtitle::_save_post()
-	 */
-	function _init() {
-		add_action( 'init', array( 'WPSubtitle', '_add_default_post_type_support' ), 5 );
-		add_action( 'add_meta_boxes', array( 'WPSubtitle', '_add_meta_boxes' ) );
-		add_action( 'save_post', array( 'WPSubtitle', '_save_post' ) );
-	}
 
 	/**
 	 * Add Default Post Type Support
@@ -54,118 +56,9 @@ class WPSubtitle {
 	 * @since  2.0
 	 * @internal
 	 */
-	function _add_default_post_type_support() {
+	static function _add_default_post_type_support() {
 		add_post_type_support( 'page', 'wps_subtitle' );
 		add_post_type_support( 'post', 'wps_subtitle' );
-	}
-
-	/**
-	 * Add Meta Boxes
-	 *
-	 * @since  2.0
-	 * @internal
-	 *
-	 * @uses  WPSubtitle::get_supported_post_types()
-	 * @uses  apply_filters( 'wps_meta_box_title' )
-	 * @uses  WPSubtitle::_add_subtitle_meta_box()
-	 */
-	function _add_meta_boxes() {
-		$post_types = WPSubtitle::get_supported_post_types();
-		foreach ( $post_types as $post_type ) {
-			$meta_box_title = apply_filters( 'wps_meta_box_title', 'Subtitle', $post_type );
-			add_meta_box( 'wps_subtitle_panel', __( $meta_box_title ), array( 'WPSubtitle', '_add_subtitle_meta_box' ), $post_type, 'normal', 'high' );
-		}
-	}
-
-	/**
-	 * Add Subtitle Meta Box
-	 *
-	 * @since  2.0
-	 * @internal
-	 *
-	 * @uses  WPSubtitle::_get_post_meta()
-	 * @uses  apply_filters( 'wps_subtitle_field_description' )
-	 */
-	function _add_subtitle_meta_box() {
-		global $post;
-		echo '<input type="hidden" name="wps_noncename" id="wps_noncename" value="' . wp_create_nonce( 'wp-subtitle' ) . '" />
-			<input type="text" id="wpsubtitle" name="wps_subtitle" value="' . WPSubtitle::_get_post_meta( $post->ID ) . '" style="width:99%;" />';
-		echo apply_filters( 'wps_subtitle_field_description', '', $post );
-	}
-
-	/**
-	 * Save Subtitle
-	 *
-	 * @since  2.0
-	 * @internal
-	 *
-	 * @uses  WPSubtitle::get_supported_post_types()
-	 *
-	 * @param  int  $post_id  Post ID or object.
-	 */
-	function _save_post( $post_id ) {
-
-		// Verify if this is an auto save routine. 
-		// If it is our form has not been submitted, so we dont want to do anything
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
-			return;
-
-		// Verify nonce
-		if ( ! WPSubtitle::_verify_posted_nonce( 'wps_noncename', 'wp-subtitle' ) )
-			return;
-
-		// Check edit capability
-		if ( ! WPSubtitle::_verify_post_edit_capability( $post_id ) )
-			return;
-	
-		// Save data
-		if ( isset( $_POST['wps_subtitle'] ) )
-			update_post_meta( $post_id, 'wps_subtitle', $_POST['wps_subtitle'] );
-	}
-
-	/**
-	 * Verify Post Edit Capability
-	 *
-	 * @since  2.0.1
-	 * @internal
-	 *
-	 * @param   int  $post_id  Post ID.
-	 * @return  bool
-	 */
-	function _verify_post_edit_capability( $post_id ) {
-		$abort = true;
-		$post_types = WPSubtitle::get_supported_post_types();
-		$post_types_obj = (array) get_post_types( array(
-			'_builtin' => false
-		), 'objects' );
-
-		// Check supported post type
-		if ( isset( $_POST['post_type'] ) && in_array( $_POST['post_type'], $post_types ) ) {
-			if ( 'page' == $_POST['post_type'] && current_user_can( 'edit_page', $post_id ) )
-				return true;
-			elseif ( 'post' == $_POST['post_type'] && current_user_can( 'edit_post', $post_id ) )
-				return true;
-			elseif ( current_user_can( $post_types_obj[$_POST['post_type']]->cap->edit_post, $post_id ) )
-				return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * Verify Posted Nonce
-	 *
-	 * @since  2.0.1
-	 * @internal
-	 *
-	 * @param   string  $nonce   Posted nonce name.
-	 * @param   string  $action  Nonce action.
-	 * @return  bool
-	 */
-	function _verify_posted_nonce( $nonce, $action ) {
-		if ( isset( $_POST[$nonce] ) && wp_verify_nonce( $_POST[$nonce], $action ) )
-			return true;
-		return false;
 	}
 
 	/**
@@ -175,15 +68,16 @@ class WPSubtitle {
 	 *
 	 * @return  array  Array of supported post types.
 	 */
-	function get_supported_post_types() {
+	static function get_supported_post_types() {
 		$post_types = (array) get_post_types( array(
 			'_builtin' => false
 		) );
 		$post_types = array_merge( $post_types, array( 'post', 'page' ) );
 		$supported = array();
 		foreach ( $post_types as $post_type ) {
-			if ( post_type_supports( $post_type, 'wps_subtitle' ) )
+			if ( post_type_supports( $post_type, 'wps_subtitle' ) ) {
 				$supported[] = $post_type;
+			}
 		}
 		return $supported;
 	}
@@ -199,7 +93,7 @@ class WPSubtitle {
 	 * @param   int|object  $post  Post ID or object.
 	 * @return  string             The filtered subtitle meta value.
 	 */
-	function get_the_subtitle( $post = 0 ) {
+	static function get_the_subtitle( $post = 0 ) {
 		$post = get_post( $post );
 		$subtitle = WPSubtitle::_get_post_meta( $post );
 		return apply_filters( 'wps_subtitle', $subtitle, $post );
@@ -214,68 +108,11 @@ class WPSubtitle {
 	 * @param   int|object  $post  Post ID or object.
 	 * @return  string             The subtitle meta value.
 	 */
-	function _get_post_meta( $id = 0 ) {
+	static function _get_post_meta( $id = 0 ) {
 		$post = get_post( $id );
 		return get_post_meta( $post->ID, 'wps_subtitle', true );
 	}
 
-}
-
-/**
- * Query DB and echo page/post subtitle, if any
- *
- * @uses  WPSubtitle::_get_post_meta()
- *
- * @since  1.0
- * @deprecated  2.0  Use get_the_subtitle() instead.
- */
-function wps_get_the_subtitle() {
-	echo WPSubtitle::_get_post_meta();
-}
-
-/**
- * Display XHTML for subtitle panel
- *
- * @since  1.0
- * @deprecated  2.0  Legacy function.
- */
-function wps_addPanelXHTML() {
-}
-
-/**
- * Include CSS for subtitle panel
- *
- * @since  1.0
- * @deprecated  2.0  Legacy function.
- */
-function wps_addPanelCSS() {
-}
-
-/**
- * Include XHTML for form inside panel
- *
- * @since  1.0
- * @deprecated  2.0  Legacy function.
- */
-function wps_showSubtitlePanel() {
-}
-
-/**
- * For pre-2.5, include shell for panel
- *
- * @since  1.0
- * @deprecated  2.0  Legacy function.
- */
-function wps_showSubtitlePanelOld() {
-}
-
-/**
- * Store subtitle content in db as custom field
- *
- * @since  1.0
- * @deprecated  2.0  Legacy function.
- */
-function wps_saveSubtitle( $post_id ) {
 }
 
 /**
@@ -314,7 +151,8 @@ function get_the_subtitle( $post = 0, $before = '', $after = '', $echo = true ) 
 		$subtitle = $before . $subtitle . $after;
 	}
 
-	if ( ! $echo )
+	if ( ! $echo ) {
 		return $subtitle;
+	}
 	echo $subtitle;
 }
